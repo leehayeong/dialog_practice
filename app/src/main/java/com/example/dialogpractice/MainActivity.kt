0446cd90a766f16fabae3ee9e44b26894fa02192
leehayeong <lhaayyy@naver.com>
2022-04-11 13:51:28 +0900

    [1] android 기본 Alert Dialog 테스트

--- app/src/main/java/com/example/dialogpractice/MainActivity.kt
+++ app/src/main/java/com/example/dialogpractice/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.dialogpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dialogpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,77 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setAlertDialog()
+        setAlertButtonDialog()
+        setAlertListDialog()
+        setAlertCheckBoxDialog()
+        setRadioButtonDialog()
+    }
+
+    private fun setAlertDialog() {
+        binding.alertDialog.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Title")
+                .setMessage("Hello, This is message")
+                .create()
+                .show()
+        }
+    }
+
+    private fun setAlertButtonDialog() {
+        binding.alertButtonDialog.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Title")
+                .setMessage("Hello, This is message")
+                .setPositiveButton("ok") { _, _ -> Log.d("hy", "positive") }
+                .setNegativeButton("cancel") { _, _ -> Log.d("hy", "negative") }
+                .setNeutralButton("neutral") { _, _ -> Log.d("hy", "neutral") }
+                .create()
+                .show()
+        }
+    }
+
+    private fun setAlertListDialog(array: Array<String> = arrayOf("A", "B", "C")) {
+        binding.alertListDialog.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("list")
+                .setItems(array) { _, which -> Log.d("hy", "currentItems : ${array[which]}") }
+                .show()
+        }
+    }
+
+    private fun setAlertCheckBoxDialog(
+        array: Array<String> = arrayOf("A", "B", "C"),
+        checkedArray: BooleanArray = booleanArrayOf(true, false, true)
+    ) {
+        binding.alertCheckBoxDialog.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("checkbox")
+                .setMultiChoiceItems(array, checkedArray) { _, which, isChecked ->
+                    Log.d("hy", "which: $which, isChecked : $isChecked")
+                }
+                .setPositiveButton("ok") { _, _ ->
+                    Log.d("hy", "checkedItems: ${checkedArray.contentToString()}")
+                }
+                .show()
+        }
+    }
+
+    private fun setRadioButtonDialog(
+        array: Array<String> = arrayOf("A", "B", "C"),
+        checkedItemPosition: Int = 0
+    ) {
+        binding.alertRadioButtonDialog.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("radio")
+                .setSingleChoiceItems(array, checkedItemPosition) { _, which ->
+                    Log.d("hy", "which: $which")
+                }
+                .setPositiveButton("ok") { _, _ ->
+                    Log.d("hy", "checkedItemPosition: $checkedItemPosition")
+                }
+                .show()
+        }
     }
 }
